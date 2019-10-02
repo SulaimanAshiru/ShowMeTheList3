33e211adc741d69e0badfb5fab2bf5ee63c1eb0f
sulaimanashiru <dash.ashiru@gmail.com>
2019-10-02 11:20:58 -0400

    added peopleadapter

--- app/src/main/java/com/example/showmethelist3/view/adapter/PersonAdapter.java
+++ app/src/main/java/com/example/showmethelist3/view/adapter/PersonAdapter.java
@@ -1,4 +1,53 @@
 package com.example.showmethelist3.view.adapter;
 
-public class PersonAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.showmethelist3.R;
+import com.example.showmethelist3.model.Person;
+
+import java.util.List;
+
+public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.PersonViewHolder> {
+
+    //Create List
+    private List<Person> personList;
+
+    //Constructor
+
+    public PersonAdapter(List<Person> personList) {
+        this.personList = personList;
+    }
+
+    @NonNull
+    @Override
+    public PersonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Create view ojbect
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.person_item_view,
+                        parent,
+                        false);
+        return new PersonViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PersonViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return personList.size();
+    }
+
+    class PersonViewHolder extends RecyclerView.ViewHolder{
+
+        public PersonViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
